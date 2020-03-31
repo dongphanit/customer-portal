6c4c0ac2f5ad21073f3139a5d4fbd0269b54ce70
dongphanit <dongphanit@gmail.com>
2020-03-31 16:13:31 +0000

    implement suggest customer, search customer by phone

--- src/Oro/Bundle/CustomerBundle/Form/Handler/FrontendCustomerUserHandler.php
+++ src/Oro/Bundle/CustomerBundle/Form/Handler/FrontendCustomerUserHandler.php
@@ -42,14 +42,6 @@ class FrontendCustomerUserHandler extends FormHandler
         $this->userManager = $userManager;
     }
 
-    function debug_to_console($data) {
-        $output = $data;
-        if (is_array($output))
-            $output = implode(',', $output);
-    
-        echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
-    }
-
     /**
      * {@inheritdoc}
      */
